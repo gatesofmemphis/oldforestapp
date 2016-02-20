70892f9c930acfe0e546c5c93896f3e5a7f1644f
Douglas Starnes <douglas.a.starnes@gmail.com>
2016-02-20 04:32:56 +0000

    fixed dumb jquery error

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-L.mapbox.accessToken = 'pk.eyJ1IjoiZG91Z2xhc2FzdGFybmVzIiwiYSI6IkxfZ1ZxN1kifQ.ycBd3UWFRS08zAJJfxGkPw';
-var overtonParkMap = L.mapbox.map('map', 'mapbox.run-bike-hike').setView([35.147203, -89.989724], 15);
-$.ajax('https://www.inaturalist.org/observations.json?swlat=35.142358&swlng=-89.996579&nelat=35.150951&nelng=-89.981602', {
-    success: function(data) {
-        var geo = JSON.parse(data);
-        var point = geo[0];
-        var lat = point.latitude;
-        var lng = point.longitude;
-        var levittShellLayer = L.mapbox.featureLayer({
-            type: 'Feature',
-            geometry: {
-                type: 'Point', 
-                coordinates: [
-                    -89.994076,
-                    35.145983
-                ]
-            }
-        });
-        levittShellLayer.on('click', function() { alert(point.iconic_taxon_name); });
-    }
+$(document).ready(function() {
+    L.mapbox.accessToken = 'pk.eyJ1IjoiZG91Z2xhc2FzdGFybmVzIiwiYSI6IkxfZ1ZxN1kifQ.ycBd3UWFRS08zAJJfxGkPw';
+    var overtonParkMap = L.mapbox.map('map', 'mapbox.run-bike-hike').setView([35.147203, -89.989724], 15);
+    $.ajax('https://www.inaturalist.org/observations.json?swlat=35.142358&swlng=-89.996579&nelat=35.150951&nelng=-89.981602', {
+        success: function(data) {
+            var point = data[0];
+            var lat = point.latitude;
+            var lng = point.longitude;
+            var levittShellLayer = L.mapbox.featureLayer({
+                type: 'Feature',
+                geometry: {
+                    type: 'Point', 
+                    coordinates: [
+                        -89.994076,
+                        35.145983
+                    ]
+                }
+            });
+            levittShellLayer.addTo(overtonParkMap);
+            levittShellLayer.on('click', function() { alert(point.iconic_taxon_name); });
+        }
+    });
 });
     
